25c2f76167bfadaa5003e773c3c2a76d2a989db4
cholo <andres.bozzani@gmail.com>
2024-08-23 15:36:49 -0300

    Curso PHP, CRUD terminado

--- CRUD/index.php
+++ CRUD/index.php
@@ -19,6 +19,7 @@
     th {
         background-color: #f2f2f2; /* Color de fondo para los encabezados */
     }
+    
 </style>
 </head>
 
@@ -28,14 +29,31 @@
     include("Conexion.php");
 
     $registro = $conexion->query("SELECT * FROM datos_clientes")->fetchAll(PDO::FETCH_OBJ);
+
+    if (isset($_POST["insertar"])){
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+        $direccion = $_POST["direccion"];
+
+        $sql = "INSERT INTO datos_clientes (nombre, apellido, direccion) VALUES (:nom, :ape, :dir)";
+
+        $result = $conexion->prepare($sql);
+        $result->execute(array(":nom"=>$nombre, ":ape"=>$apellido, ":dir"=>$direccion));
+
+        header("location:index.php");
+
+    }
     ?>
+
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 <table>
     <tr>
         <th>ID</th>
         <th>Nombre</th>
         <th>Apellido</th>
         <th>Dirección</th>
-        <th>Acciones</th>
+        <th>Eliminar</th>
+        <th>Modificar</th>
     </tr>
     <?php foreach ($registro as $persona): ?>
         <tr>
@@ -45,12 +63,22 @@
             <td><?php echo $persona->direccion; ?></td>
             <td>
                <a href="borrar_registro.php?id=<?php echo $persona->id ?>"><input type="button" name="borrar"  id="borrar" value="Borrar"></a>
-                <input type="button" name="modificar" id="modificar" value="Modificar">
+            </td>
+            <td>
+            <a href="editar_registro.php?id=<?php echo $persona->id ?>&nombre=<?php echo $persona->nombre ?>&apellido=<?php echo $persona->apellido ?>&direccion=<?php echo $persona->direccion ?> "><input type="button" name="modificar"  id="modificar" value="Modificar"></a>
             </td>
         </tr>
     <?php endforeach; ?>
-</table>
 
+    <tr>
+        <td></td>
+        <td><input type="text" name="nombre" id="nombre"></td>
+        <td><input type="text" name="apellido" id="apellido"></td>
+        <td><input type="text" name="direccion" id="direccion"></td>
+        <td><input type="submit" name="insertar" id="insertar" value="Registrar"></td>
+    </tr>
+</table>
+</form>
 
 </body>
 
