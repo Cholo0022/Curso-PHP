f9d0ee98613e8876eae48fa6fdaad0b8ae9e201c
Cholo0022 <andres.bozzani@gmail.com>
2024-09-10 22:23:01 -0300

    Curso de laravel

---
+++ Laravel/blade/resources/views/_components/card.blade.php
@@ -0,0 +1,5 @@
+<div style="border: 1px solid black; border-radius:5px; padding:10px">
+    <h3> {{$title}} </h3>
+    <img src="{{ asset("assets/img/Fiesta_Luna190.jpg") }}" alt="example" width="150px">
+    <p> {{$content}} </p>
+</div>
---
+++ Laravel/blade/resources/views/layouts/_partials/menu.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ol>
+            <li><a href="{{route("index")}}">Home</a></li>
+            <li><a href="{{route("about")}}">About</a></li>
+            <li><a href="{{route("services")}}">Services</a></li>
+            <li><a href="{{route("contact")}}">Contact</a></li>
+        </ol>
+    </nav>
+</header>
